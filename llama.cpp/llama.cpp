233db4eaeed8fe8930c43455a396cc8b1688e6dc
ahmed-kamal2004 <ahmedkamal200427@gmail.com>
2026-06-02 03:01:11 +0300

    llm: add llama.cpp commands

--- llama.cpp/llama.cpp
+++ llama.cpp/llama.cpp
@@ -1,3 +1,5 @@
+## To run from a local build, use the following command:
+
 ./build/bin/llama-server -hf ggml-org/gemma-4-E4B-it-GGUF \
              --port 8100 \
              --ctx-size 65536 \
@@ -6,3 +8,13 @@
              --webui-mcp-proxy \
              --fit on
 
+
+## To run from the system-wide installation, use the following command:
+
+llama-server \
+  -m ~/.cache/huggingface/hub/models--ggml-org--gemma-4-E4B-it-GGUF/snapshots/*/gemma-4-E4B-it-Q4_K_M.gguf \
+  --alias "ggml-org/gemma-4-E4B-it-GGUF" \
+  --port 8100 \
+  --ctx-size 65536 \
+  --reasoning-budget 512 \
+  -t 8
